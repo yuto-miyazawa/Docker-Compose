60705732ee99df7cd4d72495e77f9bb03ed06d10
宮澤　優斗　 <yuuto_miyazawa@ymiyazawa.local>
2019-11-13 18:25:37 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Docker-Compose
+docker-compose置き場
---
+++ lamp/docker/db/sql/create-table.sql
@@ -0,0 +1,13 @@
+-- tableの作成
+create table IF not exists `mst_staff`
+(
+ `id` INT(20) AUTO_INCREMENT,
+ `name` VARCHAR(20) NOT NULL,
+ `password`  VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- insert
+INSERT INTO mst_staff (name,password) VALUES ("hoge","1234");
+INSERT INTO mst_staff (name,password) VALUES ("fuga","abcd");
+INSERT INTO mst_staff (name,password) VALUES ("geoooo","ABCD");
